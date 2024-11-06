0f589e32fcb9290d1dfbaa9122838b2bbf3e4ae8
danilo90lan <15630@coderacademy.edu.au>
2024-11-06 23:35:53 +1100

    contact.html email validation and fields required attributes

--- scripts/form.js
+++ scripts/form.js
@@ -2,7 +2,17 @@
 
 window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
+        event.preventDefault(); // prevent the submit button to refresh the page
+        // check if the email field is empty
+        let emailInput  = document.getElementById("email");
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        
+        //email validation
+        if (!emailPattern.test(emailInput.value)) {
+        alert('Please enter a valid email address.');
+        return;
+        }
+
         // these IDs from the previous steps
         emailjs.sendForm('contact_service', 'contact_form', this)
             .then(() => {
