9703b7e994a39d1618b575e4836d33065290e467
danilo <danilo.lann@gmail.com>
2026-05-23 10:39:26 +1000

    Intro modified

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 const typed = new Typed('.multiple', {
-    strings: ['Full Stack Developer', 'Freelancer'],
+    strings: ['Full Stack Developer', 'Software Engineer', 'Tech Enthusiast'],
     typeSpeed:50,
     backSpeed:50,
     backDelay: 1000,
